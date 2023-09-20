a73e2860093e6a483953efb4a6ac1a9dac55a567
Your Parsa <parypary82@gmail.com>
2023-09-21 01:25:49 +0330

    make progress

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const SIZE = 600;
+const SIZE = 500;
 
 export function GRN(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -19,7 +19,7 @@ function blackOrWhite() {
 }
 
 function getCanvas(id) {
-  const canvas = document.getElementById(id);
+  const canvas = document.querySelector(`#${id}`);
   canvas.width = SIZE;
   canvas.height = SIZE;
   const ctx = canvas.getContext("2d");
@@ -27,14 +27,51 @@ function getCanvas(id) {
 }
 
 function draw(ctx, squareSize) {
-  for (let i = 0; i < SIZE; i += squareSize) {
-    for (let j = 0; j < SIZE; j += squareSize) {
-      ctx.fillStyle = blackOrWhite();
+  const colorList = [];
+
+  for (let i = 0; i < SIZE / squareSize / 2; i++) {
+    colorList[i] = [];
+    for (let j = 0; j < SIZE / squareSize / 2; j++) {
+      colorList[i].push(generateRandomColor());
+    }
+  }
+
+  console.log(colorList);
+
+  for (let i = 0; i < SIZE / 2; i += squareSize) {
+    for (let j = 0; j < SIZE / 2; j += squareSize) {
+      // console.log(i / squareSize, j / squareSize);
+      ctx.fillStyle = colorList[i / squareSize][j / squareSize];
       ctx.fillRect(i, j, squareSize, squareSize);
     }
   }
+  for (let i = SIZE / 2; i < SIZE; i += squareSize) {
+    for (let j = 0; j < SIZE / 2; j += squareSize) {
+      // console.log((SIZE - i) / squareSize - 1, j / squareSize);
+      ctx.fillStyle = colorList[(SIZE - i) / squareSize - 1][j / squareSize];
+      ctx.fillRect(i, j, squareSize, squareSize);
+    }
+  }
+  for (let i = 0; i < SIZE / 2; i += squareSize) {
+    for (let j = SIZE / 2; j < SIZE; j += squareSize) {
+      // console.log(i / squareSize, (SIZE - j) / squareSize - 1);
+      ctx.fillStyle = colorList[i / squareSize][(SIZE - j) / squareSize - 1];
+      ctx.fillRect(i, j, squareSize, squareSize);
+    }
+  }
+  for (let i = SIZE / 2; i < SIZE; i += squareSize) {
+    for (let j = SIZE / 2; j < SIZE; j += squareSize) {
+      // console.log((SIZE - i) / squareSize - 1, (SIZE - j) / squareSize - 1);
+      ctx.fillStyle =
+        colorList[(SIZE - i) / squareSize - 1][(SIZE - j) / squareSize - 1];
+      ctx.fillRect(i, j, squareSize, squareSize);
+    }
+  }
+  // requestAnimationFrame(draw);
 }
 
 const ctx = getCanvas("canvas");
 
-draw(ctx, 50);
+setInterval(() => {
+  draw(ctx, 50);
+}, 500);
